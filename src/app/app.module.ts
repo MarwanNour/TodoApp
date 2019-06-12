2722ab517b365cd23eb63097e0bce77c230b1966
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-06-12 15:16:55 +0300

    Added todo input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule,
