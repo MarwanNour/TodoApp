d730474234e68d2b2c9152e30dc8b87ac99cb943
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-06-12 13:10:07 +0300

    Modified App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TodoApp';
+export class AppComponent implements OnInit{
+  title : string;
+  
+  
+  constructor(){
+  }
+  ngOnInit(){
+    // Call a service that gets the data
+    this.title = "TodoApp";
+  }
 }
