87fb2a214115ef5e1b02f95abbc410a9f7bc642c
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-06-12 17:02:31 +0300

    todo component

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../classes/todo';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap'; 
+
+
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent implements OnInit {
-  private todo: Todo[];
 
-  constructor() { }
+
+export class TodoComponent implements OnInit {
+  // private todo: Todo[];
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
-    
+
   }
 
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
