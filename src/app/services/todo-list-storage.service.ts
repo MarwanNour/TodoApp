8573daed24dd3a3fc533856a52c5bf14def8b13f
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-06-18 13:05:20 +0300

    added service

--- src/app/services/todo-list-storage.service.ts
+++ src/app/services/todo-list-storage.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 
+const storageName = "localstorage_todo_list";
+
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class TodoListStorageService {
 
-  constructor() { }
+  private todoList;
+
+  constructor() {
+    this.todoList = JSON.parse(localStorage.getItem(storageName));
+  }
+
+  get(){
+    // the ... iterates over arrays
+    return [...this.todoList];
+  }
+
+  post(todo){
+    this.todoList.push(todo);
+    return this.get();
+  }
+
+  // updates stored list
+  private update(){
+
+  }
+  
+  put(todo, title, description, dueDate){
+    
+  }
+
+  destroy(todo){
+
+  }
+
 }
